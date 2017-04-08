6a039ac3bee41eb3359e5a33f7ca3e217d901a4f
kiddot <kiddo1024@163.com>
2017-04-08 19:58:37 +0800

    init plugins

---
+++ plugin/src/main/groovy/com.android.gradle/PluginImpl.groovy
@@ -0,0 +1,14 @@
+package com.android.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project>{
+
+    @Override
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugins"
+        }
+    }
+}
